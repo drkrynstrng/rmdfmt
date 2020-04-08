aadaa8a01eef172a914c609a85370344329bd0b6
Derek Ryan Strong <drstrong@protonmail.com>
2020-04-07 18:05:34 -0700

    Add minimal dark format

--- R/fmt.R
+++ R/fmt.R
@@ -25,3 +25,30 @@ minimal <- function(toc = TRUE, highlight = "monochrome") {
     highlight = highlight
   )
 }
+
+
+# Minimal Dark - minimalist, lightweight design with a dark theme
+
+#' @title Minimal format with a dark theme
+#'
+#' @param toc TRUE/FALSE
+#' @param highlight Syntax highlighting style
+
+minimal_dark <- function(toc = TRUE, highlight = "monochrome") {
+  # locations of resource files in the package
+  pkg_resource <- function(...) {
+    system.file(..., package = "rmdfmt")
+  }
+
+  css <- pkg_resource("rmarkdown/templates/minimal-dark/skeleton/style.css")
+
+  # call the base html_document function
+  rmarkdown::html_document(
+    toc = toc,
+    fig_width = 8,
+    fig_height = 6,
+    theme = NULL,
+    css = css,
+    highlight = highlight
+  )
+}
